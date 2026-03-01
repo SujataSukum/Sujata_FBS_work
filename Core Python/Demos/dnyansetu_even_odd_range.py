c07ff321cdfc2c2831a0ab5086240430cedb7566
SujataSukum <sujatasukum@gmail.com>
2026-03-02 00:19:34 +0530

    added one file

---
+++ Core Python/Demos/dnyansetu_even_odd_range.py
@@ -0,0 +1,14 @@
+# to find even and odd numbers between range
+
+start = int(input("Enter the starting number: "))
+end = int(input("Enter the ending number: "))
+
+print("Even numbers:")
+for num in range(start, end + 1):
+    if num % 2 == 0:
+        print(num)
+
+print("\nOdd numbers:")
+for num in range(start, end + 1):
+    if num % 2 != 0:
+        print(num)
