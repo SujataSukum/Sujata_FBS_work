10255512f2db4d969492af4b267bed9ecb5a29d3
SujataSukum <sujatasukum@gmail.com>
2026-03-23 14:17:02 +0530

    Data structure is started

---
+++ Core Python/Demos/Data Structures/List/intro.py
@@ -0,0 +1,21 @@
+# 1. Structure = []
+
+li = [10,20,30,40]
+print(type(li))
+
+# 2. Type of Data = Heterogeneous
+
+li = [10,3.14,'Firstbit Solution']                        
+print(type(li))
+print(li)
+
+# 3. Sequence = ordered
+
+# 4 . Changable = Mutable
+print(id(li))
+
+li[0]= 50
+print(id(li))
+
+print(type(li))
+print(li)
