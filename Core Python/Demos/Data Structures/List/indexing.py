7758bdb0d4970efc11eadbb4cfd62d623636553d
SujataSukum <sujatasukum@gmail.com>
2026-03-24 23:40:38 +0530

    indexing and slicing are done

---
+++ Core Python/Demos/Data Structures/List/indexing.py
@@ -0,0 +1,13 @@
+li = [10,20,30,40,50,60,70,80,90,100]
+
+
+print(li)
+print(li[1])
+print(li[3])
+print(li[-1])
+
+print(len(li))
+print(li[0])
+print(li[len(li)-1])
+
+
---
+++ Core Python/Demos/Data Structures/List/slicing.py
@@ -0,0 +1,16 @@
+li = [10,20,30,40,50,60,70,80,90,100]
+
+print(li[-3])
+print(li[1:4])
+print(li[3:8])
+print(li[1:9:2])
+print(li[4:10])
+print(li[4: :2])
+print(li)           # print whole list
+print(li[:])        # print whole list
+print(li[3:2])
+print(li[-1:-8:-2])
+print(li[10:1:-2])
+print(li[::])       # print whole list
+print(li[1::])
+print(li[::-1])     # print reverse list
